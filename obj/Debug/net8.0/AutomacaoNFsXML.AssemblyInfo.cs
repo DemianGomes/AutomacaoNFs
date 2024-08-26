fe724815eae3363b6c6d75f144ba44a112e14f6b
Demian <demian.gomes@esmenezes.com.br>
2024-08-26 15:28:26 -0300

    Arquivos de configuração

--- obj/Debug/net8.0/AutomacaoNFsXML.AssemblyInfo.cs
+++ obj/Debug/net8.0/AutomacaoNFsXML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomacaoNFsXML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94aa907099c47105caea717dace1ed2109f3cd1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7929b49df6a3dc740986ee461b74f1677169da55")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomacaoNFsXML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomacaoNFsXML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
